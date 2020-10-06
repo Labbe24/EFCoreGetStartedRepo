6fd954b1d15a43d2c53075cc4e7a31f4b058b194
Lasse Andresen <201810604@post.au.dk>
2020-10-06 13:56:44 +0200

    Add project files.

---
+++ EFCoreGetStarted/Models/Models.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreGetStarted.Models
+{
+    class Models
+    {
+    }
+
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int Price { get; set; }
+        public string Imgurl { get; set; }
+        public List<BookAuthor> BookAuthors { get; set; }
+    }
+
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Imgurl { get; set; }
+        public List<BookAuthor> BookAuthors { get; set; }
+    }
+
+    public class BookAuthor
+    {
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class BookAuthorContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Book - Author (many to many relationship)
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(p => new {p.BookId, p.AuthorId});
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ba => ba.BookId);
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Author)
+                .WithMany(a => a.BookAuthors)
+                .HasForeignKey(ba => ba.AuthorId);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("Data Source = BookSystem.db");
+        
+    }
+
+    public class Review
+    {
+
+    }
+
+    public class PriceOffer
+    {
+
+    }
+}
---
+++ EFCoreGetStarted/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreGetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
